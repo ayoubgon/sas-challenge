fb0a3ded9107e5b0c4c48309b75ab58e39d8d27d
Your Name <ayoub.chwawa@gmail.com>
2026-09-01 16:13:57 +0200

    add

---
+++ day-0/challenge.js
@@ -0,0 +1,51 @@
+//Level 1 (Beginner)
+//Age Logger
+let age = 25;
+console.log(`You are ${age} years old.`);
+
+let temperature;
+//Temperature Check
+if (temperature > 30)
+    console.log("It's hot outside!");
+else
+    console.log("The weather is nice.");
+//Even or Odd
+let number;
+    if (number % 2 != 0)
+        console.log("odd");
+    else
+        console.log("even");
+//Level 2 (Intermediate)
+//Grading System
+let score;
+if (score >= 90 && score <= 100)
+    console.log("A");
+else if (score >= 80 && score <= 89)
+    console.log("B");
+else if (score >= 70 && score <= 79)
+    console.log("C");
+else
+    console.log("F");
+//Max of Three
+let res;
+let nam1 = 5;
+let nam2 = 2;
+let nam3 = 9;
+if (nam1 >= nam2)
+    res = nam1;
+else if (nam1 <= nam2)
+    res = nam2;
+if (res >= nam3)
+    console.log("nam3")
+else
+    console.log(res);
+
+//Leap Year Checker
+let Year;
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+    console.log(true);
+else
+    console.log(false);
+
+//level 3
+//Basic Calculator
