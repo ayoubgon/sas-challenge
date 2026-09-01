bb86ce6e3bc170d99a08d12d8fb3de70a77c4c9a
Your Name <ayoub.chwawa@gmail.com>
2026-09-01 12:07:16 +0200

    let add const

---
+++ day-0/let.js
@@ -0,0 +1,39 @@
+//integer
+
+let age1 = 22;
+
+const age2 = 21;
+
+console.log(`age is ${age1} `);
+console.log(`age is ${age2} `);
+//streing 
+
+let name1 = "ayoub";
+
+const name2 = "dehbi";
+console.log(`name is ${name1} ` );
+console.log(`name is ${name2} `);
+
+
+//boolean
+
+let motivated = true;
+
+let Student = false;
+console.log('true type is  boolean');
+console.log('false type is  boolean');
+
+//undefined
+
+let un;
+console.log('let un; type is  + undefined')
+console.log("const un1; error")
+// const un1; error
+
+//null
+
+let name3 = null;
+console.log('let name3 = null; type is + null')
+
+
+
