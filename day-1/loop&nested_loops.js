ddac9ace5536c155b1a121db7aaf54726e37121a
Your Name <ayoub.chwawa@gmail.com>
2026-09-02 12:56:37 +0200

    add

---
+++ day-1/loop&nested_loops.js
@@ -0,0 +1,101 @@
+// for loops
+
+/*for (let i = 0;i < 10;i++ ){
+    console.log("namber is " + i);
+}*/
+
+/*let j = 10;
+for(;j >= 0; j++){
+    console.log("nambaer is " + j);
+}*/
+
+/*let a = 0;
+for(;a < 10 ;){
+    console.log("nambaer is " + a);
+    a++;
+}*/
+
+/*for (;;){
+    console.log("loop infine");
+}*/
+
+/*for (let b = 0;b < 10; ){
+    console.log("namber is " + b);
+    b++;
+}*/
+
+/*let k = 5;
+for (let k = 0;k < 12;k++){
+    console.log("namber is " + k);
+}*/
+
+//while loop
+
+/*while (i < 10) {
+  text += "The number is " + i;
+  console.log(text);
+  i++;
+}*/
+
+/*let games =["valoran","cs go","elden ring","lol"];
+let c = 0;
+while (game[c]){
+    console.log(game[c]);
+    c++;
+}*/
+
+/*let agel =[];
+let age = 17;
+let l = 0;
+while (age <= 18 && age >= 10){
+    agel[l] = age;
+    l++;
+    age ++;
+}*/
+//do while
+/*let t = ["ayoub","karim","ahmade"] ;
+let r = 0;
+do{
+    console.log(t[r])
+    r++;
+}*
+while(r < 3)*/
+
+//Nested Loops
+/*let u = 0;
+while (u < 10){
+    let y = 0;
+    while(y<=9){
+        console.log("y = "+ y +" u = " + u );
+        y++;
+    }
+    u++;
+}*/
+
+//break
+
+/*for(let i = 0; ;i++){
+    console.log(i);
+    if (i == 15) break;
+}
+
+switch(cars){
+    case "bmw": console.log("BMW Luxury Vehicles 1916-Present");
+        break;
+    case "nisan": console.log("Ford Mass-Market 1903-Present");;
+        break;
+    case "honda": console.log("Honda LogoHonda Mass-Market 1948-Present");
+        break;
+    case "ford": console.log("Ford Mass-Market 1903-Present");
+        break;
+}*/
+
+//continue
+/*let o = 0;
+while(o <= 30){
+    if (o % 2 == 0){
+        console.log(o);
+        continue;
+    }
+    o++;
+}*/
