a134024433aa15864a3c6ad1284ef39482fb046c
Your Name <ayoub.chwawa@gmail.com>
2026-09-02 12:57:51 +0200

    add

--- day-0/challenge.js
+++ day-0/challenge.js
@@ -91,3 +91,13 @@ switch (operator[]){
         console.log("Normal");
     else
         console.log("Overweight");
+
+
+/*
+    let year;
+if (year % 4 == 0 && year % 100 != 0)
+    console.log(true);
+else if (year % 400 == 0)
+    console.log(true);
+else
+    console.log(false);*/
